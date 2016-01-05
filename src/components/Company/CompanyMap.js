a9acf94846287b3c5627a829cf110191add21aac
ZaL <z4ls@live.com>
2016-01-06 02:51:47 +0300

    add calendar

---
+++ src/components/Company/CompanyMap.js
@@ -0,0 +1,70 @@
+'use strict';
+import React from 'react-native';
+
+const {
+  Component,
+  StyleSheet,
+  MapView,
+  View,
+  Text
+  } = React;
+
+export default class CompanyMap extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pin: this.props.pin,
+      region: this.getFitZoomMapRegionWithCoords(this.props.pin)
+    }
+  }
+
+  getFitZoomMapRegionWithCoords(region) {
+
+    var topLeftLatitude = -90;
+    var topLeftLongitude = 180;
+    var bottomRightLatitude = 90;
+    var bottomRightLongitude = -180;
+
+    topLeftLongitude = Math.min(topLeftLongitude, parseFloat(region.longitude));
+    topLeftLatitude = Math.max(topLeftLatitude, parseFloat(region.latitude));
+    bottomRightLongitude = Math.max(bottomRightLongitude, parseFloat(region.longitude));
+    bottomRightLatitude = Math.min(bottomRightLatitude, parseFloat(region.latitude));
+
+    var fitLatitude = topLeftLatitude - (topLeftLatitude - bottomRightLatitude) * 0.5;
+    var fitLongitude = topLeftLongitude + (bottomRightLongitude - topLeftLongitude) * 0.5;
+    var fitSpanLatDelta = Math.abs(topLeftLatitude - bottomRightLatitude) * 1.1;
+    var fitSpanLongDelta = Math.abs(bottomRightLongitude - topLeftLongitude) * 1.1;
+    if (fitSpanLatDelta == 0 && fitSpanLongDelta == 0) {
+      fitSpanLatDelta = fitSpanLongDelta = 0.08;
+    }
+    var fitRegion = {
+      latitude : fitLatitude,
+      longitude : fitLongitude,
+      latitudeDelta : fitSpanLatDelta,
+      longitudeDelta : fitSpanLongDelta
+    };
+
+    return fitRegion;
+  };
+
+  render() {
+    return (
+        <MapView
+          style={styles.mapContainer}
+          annotations={[this.props.pin]}
+          region={this.state.region}
+        />
+    );
+  }
+}
+
+CompanyMap.propTypes = {};
+
+let styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    marginTop:10,
+    height: 250
+  }
+});
