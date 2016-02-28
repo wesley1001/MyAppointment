35b2d998101fde72d445777573f2ab8f3ebb708d
ZaL <z4ls@live.com>
2016-02-28 19:44:02 +0300

    add confirmed appointment list

--- src/components/Appointment/ConfirmedAppointmentList.js
+++ src/components/Appointment/ConfirmedAppointmentList.js
@@ -8,18 +8,9 @@ const Actions = require('react-native-router-flux').Actions;
 
 export default class ConfirmedAppointmentList extends Component {
 
-  logout = () => {};
-
   render() {
-    const {listEmployees} = this.props;
+    const { listEmployees } = this.props;
     return (
-      //<Icon
-      //  name='ion|person'
-      //  size={40}
-      //  color={'white'}
-      //  style={{width:40,height:40,alignSelf:'center',fontWeight:100}}
-      ///>
-
       <View style={styles.cellContainer}>
         <View style={styles.cellWrapper}>
           <View style={styles.leftCol}>
@@ -33,44 +24,60 @@ export default class ConfirmedAppointmentList extends Component {
               <Text style={styles.month}>July</Text>
             </View>
             <Text style={styles.day}>5</Text>
-
             <View style={{flexDirection:'row',alignItems:'center'}}>
               <Icon
                 name='ion|clock'
                 size={20}
-                color={'purple'}
+                color={'#999999'}
                 style={{width:20,height:20,alignSelf:'center',fontWeight:100}}
               />
-              <Text style={{ paddingLeft:5,color:'purple',fontWeight:'500',fontSize:13}}>9:00pm</Text>
+              <Text style={{ paddingLeft:5,color:'#999999',fontWeight:'500',fontSize:13}}>9:00pm</Text>
             </View>
-
-          </View>
-          <View style={styles.middleCol}>
-            <Text style={styles.company}>Company name</Text>
-            <Text style={styles.service}>
-              Hair cut
-              <Text style={styles.duration}>(30min)</Text>
-            </Text>
-            <Text style={styles.employee}>faisal</Text>
-            <Text style={styles.price}>29 KD</Text>
-
           </View>
           <View style={styles.rightCol}>
-            <TouchableHighlight onPress={()=>listEmployees()} underlayColor="transparent">
-              <View style={styles.employeeSelectWrapper}>
-                <View style={{flex:2}}>
-                  <Text style={styles.employeeName} >cancel</Text>
+            <Text style={styles.company}>Company name</Text>
+            <View style={{flexDirection:'row',alignItems:'center',paddingBottom:5}}>
+              <Icon
+                name='ion|location'
+                size={15}
+                color={'purple'}
+                style={{width:15,height:15,alignSelf:'center'}}
+              />
+              <Text style={styles.location}>Salmiya</Text>
+            </View>
+            <View style={{flexDirection:'row'}}>
+              <View style={styles.middleCol}>
+                <View style={{flexDirection:'row',alignItems:'center'}}>
+                  <Text style={styles.service}>
+                    Hair cut
+                  </Text>
+                  <Text style={styles.duration}>(30min)</Text>
                 </View>
-                <View style={{flex:1}}>
-                  <Icon
-                    name='ion|close-circled'
-                    size={15}
-                    color={'white'}
-                    style={{width:15,height:15,alignSelf:'flex-end',fontWeight:'300'}}
-                  />
-                </View>
+                <Text style={styles.employee}>with faisal</Text>
               </View>
-            </TouchableHighlight>
+
+              <View style={styles.cancelWrapper}>
+                <TouchableHighlight onPress={()=>listEmployees()} underlayColor="transparent">
+
+                  <View >
+                    <Text style={styles.price}>29 KD</Text>
+                    <View style={styles.employeeSelectWrapper}>
+                      <View style={{flex:2}}>
+                        <Text style={styles.employeeName} >cancel</Text>
+                      </View>
+                      <View style={{flex:1}}>
+                        <Icon
+                          name='ion|close-circled'
+                          size={15}
+                          color={'white'}
+                          style={{width:15,height:15,fontWeight:'300'}}
+                        />
+                      </View>
+                    </View>
+                  </View>
+                </TouchableHighlight>
+              </View>
+            </View>
           </View>
         </View>
       </View>
@@ -97,12 +104,14 @@ var styles = StyleSheet.create({
     padding:10,
   },
   rightCol:{
-    flex:1
+    flex:2,
+  },
+  cancelWrapper:{
+    flex:1,
+    alignSelf:'center'
   },
   middleCol:{
     flex:1.5,
-    paddingRight:10,
-    paddingLeft:10,
   },
   leftCol:{
     flex:1,
@@ -111,34 +120,44 @@ var styles = StyleSheet.create({
   },
   company: {
     fontSize:16,
-    color:'#6ed3cf',
-    fontWeight:'500'
+    color:'#300032',
+    fontWeight:'500',
+    paddingBottom:5
+  },
+  location: {
+    color:'purple',
+    fontWeight:'500',
+    fontSize:13,
   },
   service: {
-    fontSize:14,
-    color:'#9068be',
-    fontWeight:'500'
+    fontSize:16,
+    color:'#6ed3cf',
+    fontWeight:'500',
+    paddingBottom:5
   },
   duration: {
-    fontSize:12,
-    color:'gray',
-    fontWeight:'500'
+    fontSize:10,
+    color:'#06000a',
+    fontWeight:'500',
+    paddingLeft:5
   },
-  employeeName:{
+  employee:{
     fontSize:13,
-    padding:3,
     fontWeight:'700',
-    color:'white'
+    color:'#c43235'
   },
   price: {
-    fontSize:13,
-    color:'#b56969',
-    fontWeight:'700'
+    fontSize:20,
+    color:'#677077',
+    fontWeight:'700',
+    textAlign:'center',
+    paddingBottom:5
   },
   month: {
     paddingLeft:5,
     fontSize:17,
-    fontWeight:'500'
+    fontWeight:'500',
+    color:'gray'
   },
   time: {
     fontSize:11,
@@ -154,9 +173,18 @@ var styles = StyleSheet.create({
   },
   employeeSelectWrapper: {
     backgroundColor:'red',
-    padding:5,
+    paddingTop:5,
+    paddingBottom:5,
     flexDirection:'row',
     alignItems:'center'
+  },
+  employeeName:{
+    fontSize:13,
+    paddingTop:3,
+    paddingBottom:3,
+    fontWeight:'700',
+    color:'white',
+    textAlign:'center'
   },
 
 });
